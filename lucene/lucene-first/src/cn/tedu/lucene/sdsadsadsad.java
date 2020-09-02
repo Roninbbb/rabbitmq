27a86d3aa75f0bfd8f1becf05cab6f823c89cc3f
陈先生 <857396723@qq.com>
2020-09-02 14:29:00 +0800

    sdsaassa

--- lucene/lucene-first/src/cn/tedu/lucene/sdsadsadsad.java
+++ lucene/lucene-first/src/cn/tedu/lucene/sdsadsadsad.java
@@ -1,4 +1,9 @@
 package cn.tedu.lucene;
 
 public class sdsadsadsad {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        //1111111111111111111111111111
+        //222222222222222222222
+    }
 }
